dadf2b41e907ac2eccf168caac49af6aae2ccc4c
VladimirDubinin <ro100v4anin@yandex.ru>
2026-02-11 13:21:49 +0300

    Интеграция Bootstrap

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
-import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
     plugins: [
