eff7cbc8cf01c0375028b2277eadb61495c6bb21
VladimirDubinin <ro100v4anin@yandex.ru>
2026-02-11 13:18:19 +0300

    Интеграция Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import {createApp} from 'vue/dist/vue.esm-bundler';
+import WelcomeComponent from "./components/WelcomeComponent.vue";
+
+const app = createApp({});
+app.component('welcome-component', WelcomeComponent)
+app.mount('#app')
