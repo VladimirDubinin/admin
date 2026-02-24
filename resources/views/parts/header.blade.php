eb87eab8258ae95c9cd9f72b1f277b6dfc235f1a
VladimirDubinin <ro100v4anin@yandex.ru>
2026-02-24 20:30:13 +0300

    Базовые стили для пользовательской части приложения

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,6 +1,15 @@
 <header>
-    @if (Auth::check())
-        <p>{{ Auth::user()->name }}</p>
-        <a href="{{ route('logout') }}">Выйти</a>
-    @endif
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-6 col-sm-10"></div>
+            <div class="login col-6 col-sm-2">
+                @if (Auth::check())
+                    <span>{{ Auth::user()->name }}</span>
+                    <a href="{{ route('logout') }}">Выйти</a>
+                @else
+                    <a href="{{ route('login') }}">Войти</a>
+                @endif
+            </div>
+        </div>
+    </div>
 </header>
