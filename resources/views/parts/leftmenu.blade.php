c3b48b815564b26d424c6b9dd7ddedb4bcdc7af3
VladimirDubinin <ro100v4anin@yandex.ru>
2026-02-24 20:05:17 +0300

    Добавлен fontawesome

--- resources/views/parts/leftmenu.blade.php
+++ resources/views/parts/leftmenu.blade.php
@@ -1,8 +1,18 @@
 <ul class="nav flex-column">
     <li class="nav-item {{ Request::is('admin') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('admin.index') }}">Главная</a>
+        <a class="nav-link" href="{{ route('admin.index') }}">
+            <span class="icon">
+                <i class="fas fa-th-list"></i>
+            </span>
+            Главная
+        </a>
     </li>
     <li class="nav-item {{ Request::is('admin/users*') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('admin.users') }}">Пользователи</a>
+        <a class="nav-link" href="{{ route('admin.users') }}">
+            <span class="icon">
+                <i class="fas fa-solid fa-th-list"></i>
+            </span>
+            Пользователи
+        </a>
     </li>
 </ul>
