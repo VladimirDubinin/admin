080205f48e70264ec548e2c9aff525ab6cf0a2de
VladimirDubinin <ro100v4anin@yandex.ru>
2026-04-20 10:39:55 +0300

    Логика выгрузки таблицы в Excel вынесена в абстрактный класс

---
+++ src/Shared/Application/ExcelService.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application;
+
+use Illuminate\Support\Collection;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+abstract class ExcelService
+{
+    protected Collection $items;
+    protected Worksheet $worksheet;
+
+    public function __construct(
+        private readonly Spreadsheet $sheet,
+    ) {
+        $this->worksheet = $this->sheet->getActiveSheet();
+    }
+
+    public function setItems(Collection $users): static
+    {
+        $this->items = $users;
+        return $this;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->worksheet->setTitle($title);
+        return $this;
+    }
+
+    /**
+     * Метод должен возвращать массив с определением полей алиас => названиеСвойства
+     *
+     * @return array
+     */
+    abstract protected function getFieldsDefinition(): array;
+
+    /**
+     * Метод подготавливает данные для отображения на листе Excel
+     *
+     * @return void
+     */
+    public function export(): void
+    {
+        $data = $this->getFieldsDefinition();
+        $this->worksheet->fromArray(array_values($data));
+
+        $excelData = [];
+        foreach ($this->items as $item) {
+            $itemData = [];
+            foreach ($data as $field => $alias) {
+                $itemData[] = $item->$field ?? '';
+            }
+            $excelData[] = $itemData;
+        }
+
+
+        $this->worksheet->fromArray($excelData, null, 'A2');
+        $this->write();
+    }
+
+    public function write(): void
+    {
+        $writer = new Xlsx($this->sheet);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="uses.xlsx"');
+        $writer->save('php://output');
+    }
+}
--- src/Users/Application/Services/UserExcelService.php
+++ src/Users/Application/Services/UserExcelService.php
@@ -1,47 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Users\Application\Services;
 
-use Illuminate\Support\Collection;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use App\Shared\Application\ExcelService;
 
-class UserExcelService
+class UserExcelService extends ExcelService
 {
-    private Collection $users;
-    private Worksheet $worksheet;
-
-    public function __construct(
-        private readonly Spreadsheet $sheet,
-    ) {
-        $this->worksheet = $this->sheet->getActiveSheet();
-        $this->worksheet->setTitle('Список пользователей');
-        $headerData = ["Имя", "Email", "Роль", "Дата регистрации"];
-        $this->worksheet->fromArray($headerData);
-    }
-
-    public function setItems(Collection $users): self
+    protected function getFieldsDefinition(): array
     {
-        $this->users = $users;
-        return $this;
-    }
-    public function export(): void
-    {
-        $excelData = [];
-        foreach ($this->users as $user) {
-            $excelData[] = [
-                $user->name,
-                $user->email,
-                $user->roles,
-                $user->created_at,
-            ];
-        }
-        $this->worksheet->fromArray($excelData, null, 'A2');
-
-        $writer = new Xlsx($this->sheet);
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="uses.xlsx"');
-        $writer->save('php://output');
+        return [
+            "name" => "Имя",
+            "email" => "Email",
+            "roles" => "Роль",
+            "created_at" => "Дата регистрации",
+        ];
     }
 }
